fbad2f76b17eb0c44a4e50124e122dad728d1b3e
HX <40449861+acodingpuppy@users.noreply.github.com>
2024-01-06 21:20:39 -0800

    Update uci_mnar.py

--- real data analysis/uci_mnar.py
+++ real data analysis/uci_mnar.py
@@ -32,19 +32,15 @@ for run in range(runs):
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
     data = np.array(pd.read_csv(url, low_memory=False, sep=';'))#,.; for banknote, wine
     # ---- drop the classification attribute
-    data = data[:, :-1]#[:, 2:]
-    ###
+    data = data[:, :-1]
     data = data.astype(float)
-    ###
     # ---- standardize data
     # important*** standardize first
     data = data - np.mean(data, axis=0)
     data = data / np.std(data, axis=0)
-    # ----
     
-    # nsc on uci data
-    X, R = sim_data.simulate_missing(X = data, missing_type = "general MNAR",censor_linearity = "linear", seed = run) #dimH = n_hidden,#same seed 0 # default : linear censoring#, missing_type = "MCAR":bad performance
-
+    # mnar on uci data
+    X, R = sim_data.simulate_missing(X = data, missing_type = "general MNAR",censor_linearity = "linear", seed = run) 
     data, Xnan, Xz, _ = sim_data.get_missingdata(X, R) #data is all observed for R not all 0, Xnan: with nan, Xz: replace nan with 0s
 
     N, D = data.shape
@@ -56,24 +52,31 @@ for run in range(runs):
     data = data[p, :]
     Xnan = Xnan[p, :]
     Xz = Xz[p, :]
-# =============================================================================
-#     # ---- introduce missing process
-#     Xnan, Xz = introduce_mising(data)
-# =============================================================================
+
     S = np.array(~np.isnan(Xnan), dtype=float)
     data_S = np.concatenate((data, S), axis = 1)
 
-# =============================================================================
-#     train_id = random.sample(list(range(N)), int(N*0.9))#without replacement.
-#     val_id = [id for id in list(range(N)) if id not in train_id]
-# =============================================================================
-
     # training data 
-    #Xtrain = data[train_id,:]
-    Xtrainnan = Xnan#[train_id,:]
-    Xtrainz = Xz#[train_id,:]
+    Xtrainnan = Xnan
+    Xtrainz = Xz
 
     # validation data 
-    #Xval = data[val_id,:]
-    Xvalnan = Xnan#[val_id,:]
-    Xvalz = Xz#[val_id,:]
+    Xvalnan = Xnan
+    Xvalz = Xz
+
+    print("fitting new Model")
+    # ---------------------- #
+    # ---- fit new model---- #
+    # ---------------------- #
+    model = newModel(Xtrainnan, Xvalnan, n_latent=dl, n_samples=n_samples, n_hidden=n_hidden, missing_process = 'linear_nsc', name=name)
+
+    # ---- do the training
+    trainer.train(model, batch_size=batch_size, max_iter=max_iter, name=name + 'model')
+
+    # ---- find imputation RMSE
+    #RMSE_model_ez.append(utils.imputationRMSE(model, Xtrain, Xz, Xnan, S, L)[0])
+    RMSE_model.append(utils.not_imputationRMSE(model, data, Xz, Xnan, S, L)[0])
+    
+    #print(RMSE_model_ez)
+    print(RMSE_model)
+    print("RMSE_model = {0:.5f} +- {1:.5f}".format(np.mean(RMSE_model), np.std(RMSE_model)))
