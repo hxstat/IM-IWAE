2e21c695d76ef54320d1d5c347bf14f58ab5a784
HX <40449861+acodingpuppy@users.noreply.github.com>
2024-01-06 19:42:58 -0800

    Create trainer.py

---
+++ trainer.py
@@ -0,0 +1,34 @@
+import time
+import sys
+
+
+def train(model, batch_size, max_iter=10000, name=None):
+
+    if name is not None:
+        model.save(name)
+
+    start = time.time()
+    best = float("inf")
+    
+    cnt = 0
+    for i in range(max_iter):
+        loss = model.train_batch(batch_size=batch_size)
+    
+        if i % 100 == 0:
+            took = time.time() - start
+            start = time.time()
+    
+            val_loss = model.val_batch()
+    
+            if val_loss < best and name is not None:
+                cnt = 0
+                best = val_loss
+                model.save(name)
+            else:
+                cnt += 1
+                
+            # print loss
+            if i % 1000 == 0:
+                print("{0}/{1} updates, {2:.2f} s, {3:.2f} train_loss, {4:.2f} val_loss".format(i, max_iter, took, loss, val_loss))
+                sys.stdout.flush()
+
